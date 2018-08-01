cda9d7f0909340fc4d952ddb748f628ee5ad61a7
Matthias Möller <m_moeller@live.de>
2018-08-01 15:44:57 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,56 @@
+# Gimp Plugin for SVG Masks
+
+![Example](./images/example_banner.svg)
+
+As you may know, the _jpeg_ file format cannot save an alpha-channel, which is often used for transparency, of any image.
+Another popular format, _png_, is able to save the alpha channel, but it is
+a lossless compression and the same image can occupy much more space.
+
+A lot of image process program, i.e. modern Browsers (prior IE8), also support _svg_ files.
+With this file format, it is possible to decompose an image to two images, one defining the color and one defining the alpha channel.
+This allows you to use a higher compressions from i.e. jpeg but also having transparency in your image.
+
+This Gimp Plugin helps you, creating such a svg file from an image with alpha channel.
+
+The top image is such a svg file (see [Image Source](./images/example_banner.svg) for more details).
+
+## Modes
+
+There are two modes, one mode, which creates an output color image, the mask containing
+the alpha channel and the svg file which stiches everything together.
+
+The other mode, _embed_, saves everything in the svg file.
+
+- The first mode **not embed** requires ~30% less space than the second mode, but you have to keep all the files. The produces svg just links to the images.
+
+- The second mode **embed** requires more space, but everything is encoded in the svg file.
+
+## Space Consumption Comparison
+
+Some results from the website [clipartpng.com](https://clipartpng.com/) (first found using a search engine):
+
+format      | [Cat](https://clipartpng.com/?2690,cat-png-clip-art) | [White Bear](https://clipartpng.com/?1044,angry-white-bear-png-clipart)|[Baby Fox](https://clipartpng.com/?2277,baby-fox-png-clip-art)|[Brown Bear](https://clipartpng.com/?2279,bear-brown-png-clip-art)|[Earth](https://clipartpng.com/?2279,bear-brown-png-clip-art)|[Trash Bin](https://clipartpng.com/?2129,garbage-trash-bin-with-recycle-symbol-png-clip-art)|
+|---|---|---|---|---|---|---|
+**png**         | 11.172kb      | 4.151kb       | 4.030kb       | 824kb         | 3.602kb       | 2.152kb       |
+**svg+jpeg+png**| 2.001kb (~18%) | 573kb (~14%) | 833kb (~21%)   | 251kb (~30%)  | 1.342kb (~37%)| 1.428kb (~66%) |
+**embed svg**   | 2.813kb (~25%) | 762kb (~18%) | 1.109kb (~28%) | 332kb (~40%)  | 1.787kb (~50%)| 1.903kb (~88%) |
+**svgz**        | 1.961kb (~18%)  | 524kb (~13%) |  820kb (~20%)| 235kb (~29%)| 926kb (~26%) | 864kb (~40%) |
+
+The _svgz_ file format is a compressed embed svg file (using gzip method).
+The plugin does not support creating _svgz_ out of the box, but you can use Inkscape, 7-zip or any other compression program which has gzip support.
+
+All images where exported as jpeg for the color image and png for the mask image (jpeg can also be used as a mask image).
+All jepg/gzip compressions where done with GIMP 2.10 default setting (quality 90/0.9) and 7-zip gzip method default settings.
+
+## How to Install/Use
+
+Place the [python script file](./plug-ins/masked_svg.py) under plug-ins in the GIMP plugin folder.
+The directory is usually located somewhere:
+- Windows: %AppData%\Roaming\GIMP\<Version>\plug-ins
+- Linux (GIMP 2.10): ~/.config/GIMP/2.10/plug-ins
+
+You can find a new option now under the menu: _Image->Export with mask..._
+
+## License
+
+[MIT-License](./LICENSE)
---
+++ plug-ins/masked_svg.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+from gimpfu import *
+
+from base64 import b64encode
+from os.path import splitext
+
+
+
+svg_preamble = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
+svg_doctype = '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'
+
+def img_reference(filename):
+    return filename
+
+def img_embedded(filename):
+    with open(filename, "rb") as f:
+        text = b64encode(f.read())
+        return 'data:image/{};base64,{}'.format(splitext(filename)[1][1:], text.decode('ascii'))
+
+def write_mask(io, file_name_mask, im_size, img_write_method):
+     io.write("""  <mask id="Mask">  
+     <image x="0" y="0" width="{}" height="{}" xlink:href="{}"/>
+   </mask>\n""".format(im_size[0], im_size[1], img_write_method(file_name_mask)))
+
+def write_img(io, file_name_im, im_size, img_write_method):
+    io.write('  <image x="0" y="0" width="{}" height="{}" mask="url(#Mask)" xlink:href="{}"/>\n'.format(im_size[0], im_size[1], img_write_method(file_name_im)))
+
+
+def svg_mask_image(output_name, img_name, mask_name, size, img_write_method):
+    """ Creates an svg image with a mask
+    """
+    with open(output_name, "w") as f:
+        f.write(svg_preamble)
+        f.write(svg_doctype)
+        f.write('<svg width="{}" height="{}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n'.format(size[0], size[1]))
+        write_mask(f, mask_name, size, img_write_method)
+        write_img(f, img_name, size, img_write_method)
+        f.write('</svg>\n')
+
+
+def plugin_main(img, drawable, jpeg_output, mask_output, svg_output, embed):
+
+    img_write_method = img_reference
+    if embed:
+        img_write_method = img_embedded
+
+    mask = pdb.gimp_layer_create_mask(img.active_layer, ADD_MASK_ALPHA)
+    # save files with default jpg/png settings
+    pdb.gimp_file_save(img, drawable, jpeg_output, jpeg_output)#, 0.9, 0.0, 2, 0, "", 0, 0, 0, 0)
+    pdb.gimp_file_save(img, mask, mask_output, mask_output)#, False, 9, True, True, True, True, True)
+    svg_mask_image(svg_output, jpeg_output, mask_output, (drawable.width, drawable.height), img_write_method)
+
+register(
+        "svg_mask_exporter",
+        "Exports an jpg image with a png-image mask as alpha channel in a svg file",
+        """Exports an jpg image with a png-image mask as alpha channel in a svg file.
+        Therefore, you can use the compression of a jpg file but also use the transperency, which is
+        usually not supported by jpg.
+        There are two modes, embed and non-embed. embed writes the jpeg image and the mask into the svg files while
+        non-embed mode just reference to the image files. embed files consumes ~30% more space, but you dont need the
+        intermediate jpeg/png image.""",
+        "Matthias Moeller",
+        "Matthias Moeller",
+        "2018",
+        "<Image>/Image/Export with alpha mask ...",
+        "RGBA, GRAYA",
+        [
+            #(PF_IMAGE, "img", "input image", None),
+            #(PF_DRAWABLE, "drawable", "input drawable", None),
+            (PF_FILENAME, "jpeg_output" ,"jpeg outputfile", "image.jpg"),
+            (PF_FILENAME, "mask_output", "mask outputfile", "mask.png"),
+            (PF_FILENAME, "svg_output", "svg outputfile", "output.svg"),
+            (PF_TOGGLE, "embed", "embed images into svg file", False)
+        ],
+        [],
+        plugin_main)
+
+main()
