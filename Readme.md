fcc2fd493923bdf6b238572cf428cad5735db2e0
Matthias Möller <m_moeller@live.de>
2023-03-25 19:49:48 +0100

    adds svgz support

--- Readme.md
+++ Readme.md
@@ -17,16 +17,29 @@ This Gimp Plugin helps you, creating such a svg file from an image with alpha ch
 
 The top image is such a svg file (see [Image Source](./images/example_banner.svg) for more details).
 
+## How to Install/Use
+
+Place the [python script file](./plug-ins/masked_svg.py) under plug-ins in the GIMP plugin folder.
+The directory is usually located somewhere:
+- Windows: %AppData%\Roaming\GIMP\\<Version\>\plug-ins
+- Linux (GIMP 2.10): ~/.config/GIMP/2.10/plug-ins
+
+You can find a new option now under the menu: _Image->Export with alpha mask..._  
+
+The png/jpeg files are saved using your in GIMP configured defaults for saving these formats.
+
 ## Modes
 
-There are two modes, one mode, which creates an output color image, the mask containing
-the alpha channel and the svg file which stiches everything together.
+There are three modes, one mode, which creates an output color image, the mask containing
+the alpha channel and the svg file which stiches everything together. 
 
-The other mode, _embed_, saves everything in the svg file.
+The second mode, _embed_, saves everything in the svg file. The third mode is like embed, it just compresses the file.
 
 - The first mode, **not embed**, requires ~30% less space than the second mode, but you have to keep all the files. The produces svg just links to the images.
 
 - The second mode, **embed**, requires more space, but everything is encoded in the svg file.
+
+- The third mode compresses the embed file with gzip, as browser usually support the svgz format. See [wikipedia](https://en.wikipedia.org/wiki/SVG#compression).
 
 ## Space Consumption Comparison
 
@@ -39,20 +52,10 @@ format      | [Cat](https://clipartpng.com/?2690,cat-png-clip-art) | [White Bear
 **embed svg**   | 2.813kb (~25%) | 762kb (~18%) | 1.109kb (~28%) | 332kb (~40%)  | 1.787kb (~50%)| 1.903kb (~88%) |
 **svgz**        | 1.961kb (~18%)  | 524kb (~13%) |  820kb (~20%)| 235kb (~29%)| 926kb (~26%) | 864kb (~40%) |
 
-The _svgz_ file format is a compressed embed svg file (using gzip method).
-The plugin does not support creating _svgz_ out of the box, but you can use Inkscape, 7-zip or any other compression program which has gzip support.
+The _svgz_ file format is a compressed embed svg file (using gzip method) and is usually supported by all browsers.
 
 All images where exported as jpeg for the color image and png for the mask image (jpeg can also be used as a mask image).
-All jepg/gzip compressions where done with GIMP 2.10 default setting (quality 90/0.9) and 7-zip gzip method default settings.
-
-## How to Install/Use
-
-Place the [python script file](./plug-ins/masked_svg.py) under plug-ins in the GIMP plugin folder.
-The directory is usually located somewhere:
-- Windows: %AppData%\Roaming\GIMP\<Version>\plug-ins
-- Linux (GIMP 2.10): ~/.config/GIMP/2.10/plug-ins
-
-You can find a new option now under the menu: _Image->Export with mask..._
+All jepg/gzip compressions were done with GIMP 2.10 default setting (quality 90/0.9) and 7-zip gzip method default settings.
 
 ## License
 
--- plug-ins/masked_svg.py
+++ plug-ins/masked_svg.py
@@ -4,14 +4,18 @@ from gimpfu import *
 
 from base64 import b64encode
 from os.path import splitext
-
-
+import gzip
 
 svg_preamble = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
 svg_doctype = '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'
 
 def img_reference(filename):
     return filename
+
+def get_writer(filename, use_gzip):
+    if (use_gzip):
+        return gzip.open(filename, "w")
+    return open(filename, "w")
 
 def img_embedded(filename):
     with open(filename, "rb") as f:
@@ -27,10 +31,10 @@ def write_img(io, file_name_im, im_size, img_write_method):
     io.write('  <image x="0" y="0" width="{}" height="{}" mask="url(#Mask)" xlink:href="{}"/>\n'.format(im_size[0], im_size[1], img_write_method(file_name_im)))
 
 
-def svg_mask_image(output_name, img_name, mask_name, size, img_write_method):
+def svg_mask_image(output_name, img_name, mask_name, size, img_write_method, use_gzip):
     """ Creates an svg image with a mask
     """
-    with open(output_name, "w") as f:
+    with get_writer(output_name, use_gzip) as f:
         f.write(svg_preamble)
         f.write(svg_doctype)
         f.write('<svg width="{}" height="{}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n'.format(size[0], size[1]))
@@ -39,9 +43,11 @@ def svg_mask_image(output_name, img_name, mask_name, size, img_write_method):
         f.write('</svg>\n')
 
 
-def plugin_main(img, drawable, jpeg_output, mask_output, svg_output, embed):
+def plugin_main(img, drawable, jpeg_output, mask_output, svg_output, embed, svgz):
 
     img_write_method = img_reference
+    use_gzip = svgz
+    embed = embed or use_gzip
     if embed:
         img_write_method = img_embedded
 
@@ -51,12 +57,12 @@ def plugin_main(img, drawable, jpeg_output, mask_output, svg_output, embed):
     # save files with default jpg/png settings
     pdb.gimp_file_save(img, drawable, jpeg_output, jpeg_output)
     pdb.gimp_file_save(img, mask, mask_output, mask_output)
-    svg_mask_image(svg_output, jpeg_output, mask_output, (drawable.width, drawable.height), img_write_method)
+    svg_mask_image(svg_output, jpeg_output, mask_output, (drawable.width, drawable.height), img_write_method, use_gzip)
 
 register(
         "svg_mask_exporter",
-        "Exports an jpg image with a png-image mask as alpha channel in a svg file",
-        """Exports an jpg image with a png-image mask as alpha channel in a svg file.
+        "Exports a jpg image with a png-image mask as alpha channel in a svg file",
+        """Exports a jpg image with a png-image mask as alpha channel in a svg file.
         Therefore, you can use the compression of a jpg file but also use the transperency, which is
         usually not supported by jpg.
         There are two modes, embed and non-embed. embed writes the jpeg image and the mask into the svg files while
@@ -71,7 +77,8 @@ register(
             (PF_FILENAME, "jpeg_output" ,"jpeg out file", "image.jpg"),
             (PF_FILENAME, "mask_output", "mask out file", "mask.png"),
             (PF_FILENAME, "svg_output", "svg out file", "output.svg"),
-            (PF_TOGGLE, "embed", "Embed images into svg file", True)
+            (PF_TOGGLE, "embed", "Embed images into svg file", True),
+            (PF_TOGGLE, "svgz", "Save embedding as svgz (enables embed option implicit)", False)
         ],
         [],
         plugin_main)
